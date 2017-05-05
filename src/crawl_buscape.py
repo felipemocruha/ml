2f051b88803da0441da921510966334a76189dc5
felipemocruha <felipemocruha@gmail.com>
2017-05-05 12:16:17 -0300

    yay

--- src/crawl_buscape.py
+++ src/crawl_buscape.py
@@ -1,14 +1,67 @@
+import requests
+import json
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from cytoolz import *
-from concurrent.futures import ThreadPoolExecutor
+from queue import Queue
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from random import choice, randint
+from bs4 import BeautifulSoup
+
+
+UA_LIST = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36']
+
+
+def make_product(desc, image, price):
+    print('[*] Making one product...')
+    return {'description': desc,
+            'image': image,
+            'price': price}
+
+
+def extract_product_list(li):
+    product_list = []
+    for product in li:
+        container = product.find('div', class_='image')
+        p = container.find('img')
+        desc = p.get('alt')
+        image = p.get('src')
+        price = container.find('a').get('data-preco')
+        product_list.append(make_product(desc, image, price))
+    return product_list
+
+
+def fetch_one(url):
+    print('[*] Fetching {}'.format(url))
+    headers = {'User-Agent': choice(UA_LIST)}
+    r = requests.get(url, headers=headers)
+    if r.ok:
+        print('[*] Request {} OK!'.format(url))
+        soup = BeautifulSoup(r.text, 'html5lib')
+        ul = soup.find('ul', class_='bp-product-list')
+        li = ul.find_all('li', class_='product track_checkout_container offer')
+        return extract_product_list(li)
+
+
+def main(url, final):
+    for result in fetch_one(url):
+        final.append(result)
 
 
 if __name__ == '__main__':
-    caps = webdriver.DesiredCapabilities().FIREFOX
-    caps['marionette'] = False
-    d = webdriver.Firefox(capabilities=caps)
     base_url = 'http://www.buscape.com.br/xprocura?kw=&eid={}&pagina={}'
+    eid = 81
+    pages = set([base_url.format(eid, randint(1,24000)) for i in range(400)])
+
+    final = []
+    with ThreadPoolExecutor(max_workers=50) as ex:
+        tasks = as_completed([ex.submit(main, url, final) for url in pages])
+
+    products = list(concat([final]))
+    print('[*] Writing results...')
+    with open('../data/americanas/buscape.json', 'w') as f:
+        f.writelines(json.dumps({'products': products}))
+    print('[*] New data is available!')
--- src/walmart_crawler.py
+++ src/walmart_crawler.py
@@ -5,17 +5,44 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from cytoolz import *
 from concurrent.futures import ThreadPoolExecutor
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+from queue import Queue
 
 
-def crawl_one(url, targets_queue, visited):
-    d.get(url)
+def get_domain(url):
+    return urlparse(url).netloc
 
+
+def get_domain_links(domain, links):
+    return [a.get('href') for a in links if urlparse(a.get('href')).netloc == domain]
+
+
+def get_product_links(links):
+    a = [url for url in links if url[-3:] == '/pr']
+    print(a)
+    return a
+
+
+def crawl_one(driver, url, targets_queue, visited):
+    visited.append(url)
+    driver.get(url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    WebDriverWait(driver, 2000)
+    soup = BeautifulSoup(driver.page_source, 'html5lib')
+    for l in get_product_links(get_domain_links('www.walmart.com.br', soup.find_all('a'))):
+        if l not in visited:
+            targets_queue.put_nowait(l)
 
 
 if __name__ == '__main__':
     visited = []
+    targets_queue = Queue()
     caps = webdriver.DesiredCapabilities().FIREFOX
     caps['marionette'] = False
     d = webdriver.Firefox(capabilities=caps)
-    with open('../data/walmart_crawler_targets.txt') as f:
-        targets = set([url[:-1] for url in f.readlines()])
+    #d = webdriver.PhantomJS(executable_path='../node_modules/phantomjs/bin/phantomjs')
+
+    with open('../data/walmart/crawler_seeds.txt') as f:
+        targets = [url[:-1] for url in f.readlines()]
+    crawl_one(d, targets[1], targets_queue, visited)
